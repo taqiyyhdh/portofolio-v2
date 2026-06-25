5b087c6a67b7c73e37ff3732ef1b7fcaa8bd40f0
taqiyyhdh <taqiyyhdh@gmail.com>
2026-06-25 21:41:56 +0700

    ✨ feat: add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Component } from 'react';
-import Header from "./components/Header";
-
 import './App.css'
+import Header from "./components/Header";
+import Home from './components/Home';
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div>
         <Header />
+        <main>
+          <Home />
+        </main>
       </div>
     )
   }
---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+import { Component } from 'react';
+
+class Home extends Component {
+  render() {
+
+    return (
+      <section id="home" className="home-section">
+        <div className="home-container">
+          <div className="home-content">
+            <h1>Taqiyyah Adha</h1>
+            <h3>Aspiring Web Developer | Frontend Enthusiast</h3>
+            
+            <ul className="home-buttons">
+              <li><a href="#" className="btn-primary">Project</a></li>
+              <li><a href="#" className="btn-secondary">Github</a></li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Home;
