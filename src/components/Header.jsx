f2960c9d3b046ab3e6feaf3490196230fa95869e
taqiyyhdh <taqiyyhdh@gmail.com>
2026-07-02 20:35:08 +0700

    ♻️ refactor: convert Header from class to functional component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,16 @@
-import { Component } from 'react';
-
-class Header extends Component {
-  render() {
-
-    return (
-      <header>
-        <nav>
-          <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#portfolio">Portofolio</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </nav>
-      </header>
-    )
-  }
-}
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#portfolio">Portofolio</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
