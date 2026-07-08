9cfbcf56332704b5a0516c1ef6a681c17a16891a
taqiyyhdh <taqiyyhdh@gmail.com>
2026-07-08 16:53:52 +0700

    💄 style: update contact section styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './styles/global.css';
 import Header from "./components/Header/Header";
 import Home from './components/Home/Home';
 import About from './components/About/About';
-import Contact from './components/Contact';
+import Contact from './components/Contact/Contact';
 import Portofolio from './components/Portofolio/Portofolio';
 import Footer from './components/Footer';
 
